33d67fae37f43fbf932c8e555155cb32a5717e25
Joseph Gailey <jgaileyiii@gmail.com>
2019-08-17 21:13:04 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,7 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-return programmer_hash[:dennis_ritchie][:languages][1][2]
+return programmer_hash[:dennis_ritchie][:languages]
 end
 
 
