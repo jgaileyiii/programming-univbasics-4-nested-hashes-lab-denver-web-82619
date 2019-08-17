ca6d668a11d4df7cfad66a3494b6ed0aacaf2019
Joseph Gailey <jgaileyiii@gmail.com>
2019-08-17 21:12:48 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,7 +57,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+return programmer_hash[:dennis_ritchie][:languages][1][2]
 end
+
 
 def adding_matz
 # add the following information to the top level of programmer_hash
